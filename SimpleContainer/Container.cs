1029b0d93b356c010cfdfa05d5a12e1b406a8eba
C. J. Anderson <chris@standsure.io>
2017-01-08 17:42:33 -0500

    Initial commit

---
+++ SimpleContainer/Container.cs
@@ -0,0 +1,112 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="Container.cs" company="Stand Sure LLC">
+//      Copyright (c) 2017 Stand Sure LLC. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+
+namespace SimpleContainer
+{
+  using System;
+  using System.Collections.Generic;
+  using System.Linq;
+  using System.Reflection;
+
+  public class Container
+  {
+    private readonly IDictionary<Type, Type> dependcies;
+
+    public Container()
+    {
+      this.dependcies = new Dictionary<Type, Type>();
+    }
+
+    public void Register<TService, TServiceProvider>() 
+      where TServiceProvider : TService
+      where TService : class
+    {
+      this.dependcies.Add(typeof(TService), typeof(TServiceProvider));
+    }
+
+    public T Resolve<T>() where T : class
+    {
+      var retval = this.Resolve(typeof(T)) as T;
+      if (retval == null)
+      {
+        retval = default(T);
+      }
+
+      return retval;
+    }
+
+    private object Resolve(Type type)
+    {
+      object retval = null;
+
+      Type providerType = this.ResolveServiceProvider(type);
+      if (providerType != null)
+      {
+        retval = this.BuildKnownType(providerType);
+      }
+      else
+      {
+        retval = this.BuildUnknownType(type);
+      }
+
+      return retval;
+    }
+
+    private Type ResolveServiceProvider(Type type)
+    {
+      return this.dependcies
+                 .Where(kvp => kvp.Key == type)
+                 .Select(kvp => kvp.Value)    
+                 .FirstOrDefault();
+    }
+
+    private object BuildKnownType(Type providerType)
+    {
+      object retval = null;
+
+      var constructor = providerType.GetConstructors()
+                                    .FirstOrDefault();
+      if (constructor != null)
+      {
+        var parameters = constructor.GetParameters();
+        if (!parameters.Any())
+        {
+          retval = Activator.CreateInstance(providerType);
+        }
+        else
+        {
+          retval = constructor.Invoke(
+            this.ResolveParameters(parameters)
+                .ToArray()
+          );
+        }
+      }
+
+      return retval;
+    }
+
+    private object BuildUnknownType(Type type)
+    {
+      object retval;
+      try
+      {
+        retval = Activator.CreateInstance(type);
+      }
+      catch
+      {
+        retval = null;
+      }
+
+      return retval;
+    }
+
+    private IEnumerable<object> ResolveParameters(IEnumerable<ParameterInfo> parameters)
+    {
+      return parameters.Select(param => this.Resolve(param.ParameterType))
+                       .ToList();
+    }
+  }
+}
---
+++ SimpleContainerTests/ContainerTests.cs
@@ -0,0 +1,105 @@
+﻿// -----------------------------------------------------------------------
+//  <copyright file="ContainerTests.cs" company="Stand Sure LLC">
+//      Copyright (c) 2017 Stand Sure LLC. All rights reserved.
+//  </copyright>
+// -----------------------------------------------------------------------
+using System.Collections.Generic;
+
+namespace SimpleContainerTests
+{
+  using NUnit.Framework;
+  using System;
+  using Container = SimpleContainer.Container;
+
+  [TestFixture()]
+  public class ContainerTests : AssertionHelper
+  {
+    Container target;
+
+    [SetUp]
+    public void SetUp()
+    {
+      this.target = new Container();
+    }
+
+    [Test]
+    public void Container_Should_Have_Register_Method()
+    {
+      var actual = target.GetType().GetMethod("Register");
+
+      this.Expect(actual, Is.Not.Null);
+    }
+
+    [Test]
+    public void Container_Should_Have_Resolve_Method()
+    {
+      var actual = target.GetType().GetMethod("Resolve");
+
+      this.Expect(actual, Is.Not.Null);
+    }
+
+    [Test]
+    public void Registered_ServiceProvider_Should_Resolve()
+    {
+      target.Register<IList<string>, List<string>>();
+
+      var actual = target.Resolve<IList<string>>();
+
+      this.Expect(actual, Is.Not.Null);
+      this.Expect(actual, Is.InstanceOf<IList<string>>());
+      this.Expect(actual, Is.InstanceOf<List<string>>());
+    }
+
+    private interface IFoo
+    {
+      void Execute();
+    }
+
+    private class Foo : IFoo
+    {
+      public Foo(string text)
+      {
+      }
+
+      public void Execute()
+      {
+      }
+    }
+
+    [Test]
+    public void Container_Should_Be_Able_To_Handle_ctor_with_Parameters()
+    {
+      target.Register<IFoo, Foo>();
+
+      var actual = target.Resolve<IFoo>();
+
+      this.Expect(actual, Is.Not.Null);
+      this.Expect(actual, Is.InstanceOf<IFoo>());
+      this.Expect(actual, Is.InstanceOf<Foo>());
+    }
+
+    private interface IBar
+    {
+    }
+
+    private class Bar : IBar
+    {
+      public Bar(IFoo foo)
+      {
+      }
+    }
+
+    [Test]
+    public void Container_Should_Be_Able_To_Handle_Nested_Known_Types()
+    {
+      target.Register<IFoo, Foo>();
+      target.Register<IBar, Bar>();
+
+      var actual = target.Resolve<IBar>();
+      this.Expect(actual, Is.Not.Null);
+      this.Expect(actual, Is.InstanceOf<IBar>());
+      this.Expect(actual, Is.InstanceOf<Bar>());
+    }
+
+  }
+}
