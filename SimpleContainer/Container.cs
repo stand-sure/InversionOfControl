9020ec855dbbd165fd1e039e01a128d6500eff4c
C. J. Anderson <chris@standsure.io>
2017-01-08 20:03:48 -0500

    XML DOC comments

--- SimpleContainer/Container.cs
+++ SimpleContainer/Container.cs
@@ -11,15 +11,29 @@ namespace SimpleContainer
   using System.Linq;
   using System.Reflection;
 
+  /// <summary>
+  /// A simple 
+  /// </summary>
   public class Container
   {
-    private readonly IDictionary<Type, Type> dependcies;
+    /// <summary>
+    /// The dependcies.
+    /// </summary>
+    private readonly IDictionary<Type, Type> dependencies;
 
+    /// <summary>
+    /// Initializes a new instance of the <see cref="T:SimpleContainer.Container"/> class.
+    /// </summary>
     public Container()
     {
-      this.dependcies = new Dictionary<Type, Type>();
+      this.dependencies = new Dictionary<Type, Type>();
     }
 
+    /// <summary>
+    /// Registers TServiceProvider as a provider for TService.
+    /// </summary>
+    /// <typeparam name="TService">The service interface type.</typeparam>
+    /// <typeparam name="TServiceProvider">The service provider type.</typeparam>
     public void Register<TService, TServiceProvider>()
       where TServiceProvider : TService
       where TService : class
@@ -34,9 +48,14 @@ namespace SimpleContainer
         throw new ArgumentException("TServiceProvider must have a public constructor");
       }
 
-      this.dependcies.Add(typeof(TService), typeof(TServiceProvider));
+      this.dependencies.Add(typeof(TService), typeof(TServiceProvider));
     }
 
+    /// <summary>
+    /// Resolves the service provider from the service interface key.
+    /// </summary>
+    /// <typeparam name="T">The type of the service.</typeparam>
+    /// <returns>An instance of the service provider.</returns>
     public T Resolve<T>() where T : class
     {
       var retval = this.Resolve(typeof(T)) as T;
@@ -48,6 +67,11 @@ namespace SimpleContainer
       return retval;
     }
 
+    /// <summary>
+    /// Resolves the specified type.
+    /// </summary>
+    /// <param name="type">The service type.</param>
+    /// <returns>An instance of the service provider type.</returns>
     private object Resolve(Type type)
     {
       object retval = null;
@@ -65,14 +89,26 @@ namespace SimpleContainer
       return retval;
     }
 
+    /// <summary>
+    /// Resolves the service provider.
+    /// </summary>
+    /// <returns>The service provider type.</returns>
+    /// <param name="type">The service type.</param>
+    /// <remarks>May return null.</remarks>
     private Type ResolveServiceProvider(Type type)
     {
-      return this.dependcies
+      return this.dependencies
                  .Where(kvp => kvp.Key == type)
                  .Select(kvp => kvp.Value)
                  .FirstOrDefault();
     }
 
+    /// <summary>
+    /// Builds a known/registered type.
+    /// </summary>
+    /// <returns>An instance of the registered type.</returns>
+    /// <param name="providerType">The provider type.</param>
+    /// <remarks>May throw an exception.</remarks>
     private object BuildKnownType(Type providerType)
     {
       object retval = null;
@@ -98,6 +134,12 @@ namespace SimpleContainer
       return retval;
     }
 
+    /// <summary>
+    /// Attempts to build unknown/unregistered type.
+    /// </summary>
+    /// <returns>The unknown type or null.</returns>
+    /// <param name="type">The Type.</param>
+    /// <remarks>Will not throw.</remarks>
     private object BuildUnknownType(Type type)
     {
       object retval;
@@ -113,6 +155,11 @@ namespace SimpleContainer
       return retval;
     }
 
+    /// <summary>
+    /// Resolves the parameters.
+    /// </summary>
+    /// <returns>The an <see cref="System.Collections.Generic.IEnumerable{Object}"/> containing instances of the parameters.</returns>
+    /// <param name="parameters">An enumerable of <see cref="System.Reflection.ParameterInfo"/></param>
     private IEnumerable<object> ResolveParameters(IEnumerable<ParameterInfo> parameters)
     {
       return parameters.Select(param => this.Resolve(param.ParameterType))
